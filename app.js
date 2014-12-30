d4ec4864c585c8fe923e30eccd9db1dd2ce28d22
gchoo <zhugino@tmail.com>
2014-12-30 16:21:14 +0800

    Add Imgs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-(function(){
-	var app = angular.module('store', [ ]);
-	app.controller('StoreController', function(){
+(function() {
+	var app = angular.module('store', []);
+	app.controller('StoreController', function() {
 		this.products = gems;
 	});
 	var gem = {
@@ -10,18 +10,35 @@
 		canPurchase: true,
 		soldOut: false
 	}
-	var gems = [
-		{
-			name: "Dodecahedron",
-			price: 2.95,
-			description: "test Dodecahedron",
-			canPurchase: true,
-		},
-		{
-			name: "Pentagonal Gem",
-			price: 5.95,
-			description: "test Pentagonal Gem",
-			canPurchase: false,
-		}
-	];
+	var gems = [{
+		name: "Dodecahedron",
+		price: 2.95,
+		description: "test Dodecahedron",
+		canPurchase: true,
+		images: [
+			{
+				full: '5769/5777512.png', 
+				thumb: '5769/5777513.png'
+			},
+			{
+				full: '5769/5777514.png', 
+				thumb: '5769/5777515.png'
+			}
+		]
+	}, {
+		name: "Pentagonal Gem",
+		price: 5.95,
+		description: "test Pentagonal Gem",
+		canPurchase: false,
+		images: [
+			{
+				full: '5769/5777516.png', 
+				thumb: '5769/5777517.png'
+			},
+			{
+				full: '5769/5777518.png', 
+				thumb: '5769/5777519.png'
+			}
+		]
+	}];
 })();
